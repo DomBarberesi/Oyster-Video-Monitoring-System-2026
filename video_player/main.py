a050bb75d57624907ec4658b7f8fc926dbe08363
Ella Kennedy <ellakennedy@Ellas-MBP-2.salisbury.edu>
2026-06-30 11:39:38 -0400

    added video viewer for testing models

---
+++ video_player/main.py
@@ -0,0 +1,47 @@
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+
+vidNum = 1
+capture = cv2.VideoCapture("oyster_videos/oyster_video_" + str(vidNum) + ".mp4")
+isFrame, frame = capture.read()
+
+#model path will need to be changed
+model = YOLO("/Users/ellakennedy/Oyster-Video-Monitoring-System-2026/YOLO_testing/2026_model/runs/detect/models/yolov8m_2026/weights/best.pt")
+
+while isFrame:
+
+    results = model.predict(frame, verbose=False)
+
+    for result in results:
+        annotator = Annotator(frame)
+
+        boxes = result.boxes
+        for box in boxes:
+            coords = box.xyxy[0]
+            cls = box.cls
+            annotator.box_label(coords, model.names[int(cls)])
+
+    frame = annotator.result()
+
+    cv2.imshow("YOLO V8 Detection", frame)
+    key = cv2.waitKey(0)
+
+    if key == ord('n'):
+        vidNum += 1
+        if vidNum > 18:
+            vidNum = 1
+        capture = cv2.VideoCapture("oyster_videos/oyster_video_" + str(vidNum) + ".mp4")
+    elif key == ord('p'):
+        vidNum -= 1
+        if vidNum < 1:
+            vidNum = 18
+        capture = cv2.VideoCapture("oyster_videos/oyster_video_" + str(vidNum) + ".mp4")
+    elif key == 27:
+        break
+
+    isFrame, frame = capture.read()
+
+
+capture.release()
+cv2.destroyAllWindows()
